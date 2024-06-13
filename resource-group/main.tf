88a8423f53b11792e0c04555bae91a2f1a23847c
sayan roy <ritarvi10@gmail.com>
2024-06-13 11:17:24 +0530

    Added files

---
+++ resource-group/main.tf
@@ -0,0 +1,4 @@
+resource "azure_resource_group" "resourcegroup" {
+    name = var.resource_group_name
+    location = var.location
+}
---
+++ resource-group/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "resourcegroupazure"
+location = "canadacentral"
