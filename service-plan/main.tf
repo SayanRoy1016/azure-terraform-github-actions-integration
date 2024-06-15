d83a836debd9f060e8f966d94da0abc2253a3ddd
sayan roy <ritarvi10@gmail.com>
2024-06-15 17:58:31 +0530

    corrected os type

--- service-plan/main.tf
+++ service-plan/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_service_plan" "serviceplan" {
   name = var.service_plan_name
   location = var.service_plan_location
   resource_group_name = var.resource_group_name
-  os_type = "linux"
+  os_type = "Linux"
   sku_name = "P1v3"
 }
